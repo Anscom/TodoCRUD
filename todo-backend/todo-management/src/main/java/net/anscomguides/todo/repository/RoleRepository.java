ff7ecb1551477e3f9b773126d4b3ac11a9e0fa29
Anscom <74126976+Anscom@users.noreply.github.com>
2024-05-19 15:21:57 +0800

    Add files via upload

---
+++ todo-backend/todo-management/src/main/java/net/anscomguides/todo/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package net.anscomguides.todo.repository;
+
+import net.anscomguides.todo.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String name);
+}
