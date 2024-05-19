62c37c6383bc77bdd5263148f5fca6ccf98c9e74
Anscom <ooicheongwen123456@gmail.com>
2024-05-19 15:43:50 +0800

    hi

--- src/main/java/net/anscomguides/todo/repository/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package net.anscomguides.todo.repository;
-
-import net.anscomguides.todo.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByName(String name);
-}
--- src/main/java/net/anscomguides/todo/repository/TodoRepository.java
+++
@@ -1,7 +0,0 @@
-package net.anscomguides.todo.repository;
-
-import net.anscomguides.todo.entity.Todo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TodoRepository extends JpaRepository<Todo, Long> {
-}
